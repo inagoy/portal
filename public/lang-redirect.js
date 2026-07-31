2ac091ce29fb889362fe5c40f3e734a532ebd803
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2026-07-31 00:07:45 -0300

    es by default

--- public/lang-redirect.js
+++ public/lang-redirect.js
@@ -1,16 +1,11 @@
 (function(){
   try {
-    var stored = null;
-    try { stored = localStorage.getItem('lang'); } catch (e) {}
-    var nav = (navigator.language || 'en').toLowerCase();
-    var lang = (stored || nav).indexOf('es') === 0 ? 'es' : 'en';
     var p = location.pathname;
     // If already under /es or /en, do nothing
     if (/^\/(es|en)(\/|$)/.test(p)) return;
-    var dest = '/' + lang + p;
+    var dest = '/es' + p + location.search + location.hash;
     location.replace(dest);
   } catch (e) {
     // Fallback: do nothing; meta refresh will handle
   }
 })();
-
